d6e77ed9b1dc40c04b8ad335cd212aedaf17d7a8
c.castro.protel <c.castro@protelcotelsa.com>
2026-09-16 16:15:03 -0500

    Revert "add: redireccion a mesas"
    This reverts commit 4f8df18dbd6c53a916d5ca0a7ad56e2470588006.

--- src/app/invitation/[id]/page.tsx
+++ src/app/invitation/[id]/page.tsx
@@ -6,11 +6,11 @@ import { Analytics } from "@vercel/analytics/next"
 export const revalidate = 0; // No cachear - actualiza siempre
 
 interface PageProps {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 }
 
 export default async function InvitationPage({ params }: PageProps) {
-  const { id } = await params;
+  const { id } = params;
   console.log('ID recibido:', id);
   // Consultar el nombre del invitado en Supabase
   const { data: guest, error } = await supabase
